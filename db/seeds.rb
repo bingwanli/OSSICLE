1878fb9c212f1c49c313428737878a07374c793f
Gini Li <bingwan.gini.li@gmail.com>
2019-12-02 18:44:43 +0800

    added html and bootstrap for index and seeded info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+require 'faker'
 Shoe.destroy_all
 10.times do
-Shoe.create({shoe_type: Faker::Dessert.variety, moodboard: Faker::Internet.url, status: "1", votes: "0", detail: Faker::TvShows::TheFreshPrinceOfBelAir.quote})
-puts "finished shoe"
-puts Shoe.create({shoe_type: Faker::Dessert.variety, moodboard: Faker::Internet.url, status: "1", votes: "0", detail: Faker::TvShows::TheFreshPrinceOfBelAir.quote})
+the_shoe = Shoe.create({
+  user_id: 1,
+  shoe_type: Faker::Dessert.variety,
+  moodboard: Faker::Internet.url,
+  status: 1,
+  votes: 0,
+  detail: Faker::TvShows::TheFreshPrinceOfBelAir.quote
+})
+puts the_shoe
+puts "uploaded"
 end
 
 
