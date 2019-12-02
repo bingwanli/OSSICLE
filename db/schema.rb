990a286cf19d98c4b179938553f6364af8ad2f94
Gini Li <bingwan.gini.li@gmail.com>
2019-12-02 14:25:15 +0800

    added portfolio to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_061941) do
+ActiveRecord::Schema.define(version: 2019_12_02_062045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_061941) do
     t.string "name"
     t.integer "designer"
     t.string "bio"
+    t.string "portfolio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
