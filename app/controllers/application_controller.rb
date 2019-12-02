1443510bbd68a1fde67e4b40b408282f8616bf4a
Gini Li <bingwan.gini.li@gmail.com>
2019-12-02 15:20:45 +0800

    added strong params for user sign up and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    # NEED TO ADD BIO AND PORTFOLIO FOR DESIGNERS LATER
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:prof_pic, :name])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:prof_pic])
+  end
+
 end
