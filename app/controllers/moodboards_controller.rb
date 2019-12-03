41dd59a16b6ddde075e081140f23b78ceaaf462a
Gini Li <bingwan.gini.li@gmail.com>
2019-12-03 15:13:20 +0800

    created routes for moodboard and added proper links in index, created shows path

--- app/controllers/moodboards_controller.rb
+++ app/controllers/moodboards_controller.rb
@@ -1,8 +1,12 @@
 class MoodboardsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @moodboards = Moodboard.all
+  end
+
+  def show
+    set_moodboard
   end
 
   def new
@@ -13,11 +17,24 @@ class MoodboardsController < ApplicationController
     @moodboard = Moodboard.new(moodboards_params)
   end
 
+  def edit
+    set_moodboard
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
 private
+
+  def set_moodboard
+    @moodboard = Moodboard.find(params[:id])
+  end
 
   def moodboards_params
     params.require(:moodboard).permit(:shoe_type, :is_finished, :detail, :votes)
   end
-
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   root to: 'pages#home'
+
   devise_for :users
-  resources :moodboards, only: [:index, :new, :create] do
-    resources :shoes, only: [:new, :create, :index, :show, :edit, :update]
+  resources :moodboards do
+    resources :shoes, only: [:new, :index, :show, :edit, :update]
   end
-  resources :shoes, only: [:delete]
+  resources :shoes, only: [:create, :delete]
 end
